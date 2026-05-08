906a18bdd9ddedf07201a5e46868b682fa07f7a3
Alexander Ela <aela@esri.com>
2026-05-08 16:47:05 -0600

    Add third passing test to arcgis-popup-panel.

--- packages/webgis-components/src/components/arcgis-popup-panel/arcgis-popup-panel.test.ts
+++ packages/webgis-components/src/components/arcgis-popup-panel/arcgis-popup-panel.test.ts
@@ -3,24 +3,23 @@ import { html, render } from 'lit';
 import './arcgis-popup-panel';
 
 describe('<arcgis-popup-panel>', () => {
+  const selection = {
+    id: '1',
+    title: 'Test Feature',
+    attributes: {
+      Country: 'United States',
+      Elevation: 3000,
+    },
+  };
+
   it('renders panel when selection is provided', async () => {
     const container = document.createElement('div');
     document.body.appendChild(container);
-    render(
-      html`<arcgis-popup-panel selection="{{}}"></arcgis-popup-panel>`,
-      container,
-    );
+    render(html`<arcgis-popup-panel></arcgis-popup-panel>`, container);
 
     const el = container.querySelector('arcgis-popup-panel');
-    el!.selection = {
-      id: '1',
-      title: 'Test Feature',
-      attributes: {
-        Country: 'United States',
-        Elevation: 3000,
-      },
-    };
 
+    el!.selection = selection;
     await el?.updateComplete;
 
     const panel = el?.shadowRoot!.querySelector('calcite-panel');
@@ -40,5 +39,23 @@ describe('<arcgis-popup-panel>', () => {
     expect(panel).toBeNull();
   });
 
-  it('emits `selected-cleared` event upon clicking close button', () => {});
+  it('emits `selected-cleared` event upon clicking close button', async () => {
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+    render(html`<arcgis-popup-panel></arcgis-popup-panel>`, container);
+
+    const el = container.querySelector('arcgis-popup-panel');
+    el!.selection = selection;
+    await el!.updateComplete;
+
+    let eventFired = false;
+    el?.addEventListener('selection-cleared', () => (eventFired = true));
+
+    const closeAction = el?.shadowRoot!.querySelector(
+      'calcite-action',
+    ) as HTMLElement;
+    closeAction.click();
+
+    expect(eventFired).toBe(true);
+  });
 });
